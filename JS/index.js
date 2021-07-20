63fe1342a0d08cb96c8464b7c0aaf5788d66e880
Spl4shh <74205736+Spl4shh@users.noreply.github.com>
2021-07-20 23:28:17 +0200

    Tentative de carousel
    Je créé un carousel sur la page d'accueil en JavaScript afin de remplacer le paragraphe pour acceder aux projets, cela permettant d'avoir un site plus convivial.
    Plusieurs beug :
    - Probleme lors de l'index 3 pour un retour en arriere, la 2eme condition est verifiée, peut etre l'enlever ? Ou alors supprimer la fonction go to ? ou faire la modif de l'index dans les fonctions prev et next ?
    - le nombre d'element affiché qui beug si ce n'est pas 2 ou 3

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
+/*******************************************************************/
+// Souligner les bouton
+
+
 var listeArticle = document.querySelectorAll(".article_text");
 var listeBouton = document.querySelectorAll(".bouton");
 
@@ -15,3 +19,129 @@ listeBouton.forEach(bouton =>
     });
 
 
+/*******************************************************************/
+// Carousel
+
+
+class Carousel
+{
+    /**
+     * 
+     * @param {HTMLElement} element 
+     * @param {Object} options 
+     * @param {Object} options.slidesToScroll Nombre d'element à faire defiler
+     * @param {Object} options.slidesVisible  Nombre d'element visible dans un slide
+     */
+    constructor (element, options = {})
+    {
+        this.element = element;
+        this.options = Object.assign({}, 
+        {
+            slidesToScroll: 1,
+            slidesVisible: 2   
+        }, options);
+        let children = [].slice.call(element.children);
+
+        this.currentItem = 0;
+        this.root = this.createDivWithClass("carousel");
+        this.container = this.createDivWithClass("carousel_container");
+
+
+        this.root.appendChild(this.container);
+        this.element.appendChild(this.root)
+
+        this.items = children.map(child => 
+        {
+            let item = this.createDivWithClass("carousel_item");
+            item.appendChild(child);
+            this.container.appendChild(child);
+            return item;
+        });
+
+        console.log(this.items);
+        this.setStyle();
+        this.createNaviguation();
+    }
+
+
+    setStyle ()
+    {
+        let ratio = this.items.length / this.options.slidesVisible;
+        this.container.style.width = (ratio * 100) + "%";
+        this.items.forEach(item => 
+        {
+            item.style.width = ((100 / this.options.slidesVisible) / ratio) + "%"
+        });
+        console.log(ratio);
+    }
+
+
+    createNaviguation()
+    {
+        let nextButton = this.createDivWithClass("carousel_next");
+        let prevButton = this.createDivWithClass("carousel_prev");
+        this.root.appendChild(nextButton);
+        this.root.appendChild(prevButton);
+
+        nextButton.addEventListener("click", this.next.bind(this));
+        prevButton.addEventListener("click", this.prev.bind(this));
+    }
+
+    next()
+    {
+        this.goToItem(this.currentItem + this.options.slidesToScroll)
+    }
+
+    prev()
+    {
+        this.goToItem(this.currentItem - this.options.slidesToScroll)
+    }
+
+    /**
+     * Deplace le carousel vers l'element ciblé
+     * 
+     * @param {Number} index
+     */
+    goToItem(index)
+    {
+        console.log(index + " Index entrée");
+        console.log(this.items.length + " Item length");
+                
+        if(index < 0)
+        {
+            index = this.items.length - this.options.slidesVisible;
+            console.log("Je suis en haut abruti");
+        } else if (index >= this.items.length || this.items[this.currentItem + this.options.slidesVisible] === undefined)
+        {
+            index = 0;
+        }
+
+        console.log(index + " index sortie");
+
+        let translateX = index * -100 / this.items.length;
+
+        this.container.style.transform = "translate3d(" + translateX + "%, 0, 0)";
+        this.currentItem = index;
+    }
+
+    /**
+     * 
+     * @param {string} className 
+     * @returns {HTMLElement}
+     */
+    createDivWithClass(className)
+    {
+        let div = document.createElement("div");
+        div.setAttribute("class", className);
+        return div;
+    }
+
+
+
+
+}
+
+new Carousel (document.querySelector("#carousel"), {
+    slidesToScroll: 1,
+    slidesVisible: 2
+})
